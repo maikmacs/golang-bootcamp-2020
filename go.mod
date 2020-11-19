8150cba06c56e3b3c05da543f2c2b0ca29a6bcfd
maikmacs <maik.macs@hotmail.com>
2020-11-18 20:06:53 -0600

    Write CSV Functions

--- go.mod
+++ go.mod
@@ -3,16 +3,21 @@ module golang-bootcamp
 go 1.15
 
 require (
+	github.com/cosmtrek/air v1.21.2 // indirect
+	github.com/creack/pty v1.1.11 // indirect
+	github.com/fatih/color v1.10.0 // indirect
 	github.com/gin-gonic/gin v1.6.3
 	github.com/go-playground/validator/v10 v10.4.1 // indirect
 	github.com/golang/protobuf v1.4.3 // indirect
+	github.com/imdario/mergo v0.3.11 // indirect
 	github.com/json-iterator/go v1.1.10 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.1 // indirect
+	github.com/pelletier/go-toml v1.8.1 // indirect
 	github.com/spf13/viper v1.7.1
 	github.com/ugorji/go v1.2.0 // indirect
 	golang.org/x/crypto v0.0.0-20201116153603-4be66e5b6582 // indirect
-	golang.org/x/sys v0.0.0-20201113233024-12cec1faf1ba // indirect
+	golang.org/x/sys v0.0.0-20201118182958-a01c418693c7 // indirect
 	google.golang.org/protobuf v1.25.0 // indirect
 	gopkg.in/yaml.v2 v2.3.0 // indirect
 )
--- interface/controllers/syncData.go
+++ interface/controllers/syncData.go
@@ -1,16 +1,77 @@
 package controllers
 
 import (
+	"encoding/csv"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
+	"os"
+	"strconv"
+
+	"golang-bootcamp/config"
 
 	"github.com/gin-gonic/gin"
 )
 
 type SyncDataController struct{}
 
-func (s SyncDataController) Status(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{
-		"message": "Sync Controller",
-	})
+type Episode struct {
+	Name    string `json:"name"`
+	Season  int    `json:"season"`
+	Number  int    `json:"number"`
+	Airdate string `json:"airdate"`
+}
 
+func (s SyncDataController) Status(c *gin.Context) {
+	config := config.GetConfig()
+	response, err := http.Get(config.GetString("data.endPoint"))
+
+	if err != nil {
+		fmt.Print(err.Error())
+		os.Exit(1)
+	}
+
+	responseData, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"message": "Failed sync",
+		})
+
+	} else {
+		var episodes []Episode
+
+		json.Unmarshal(responseData, &episodes)
+
+		file, err := os.Create(config.GetString("data.outputPath"))
+		checkError("Cannot create file", err)
+		defer file.Close()
+
+		writer := csv.NewWriter(file)
+		defer writer.Flush()
+
+		for _, episode := range episodes {
+			var row []string
+
+			row = append(row, episode.Name)
+			row = append(row, strconv.Itoa(episode.Season))
+			row = append(row, strconv.Itoa(episode.Number))
+			row = append(row, episode.Airdate)
+
+			err := writer.Write(row)
+			checkError("Cannot write to file", err)
+		}
+
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Data synced",
+		})
+	}
+
+}
+
+func checkError(message string, err error) {
+	if err != nil {
+		log.Fatal(message, err)
+	}
 }
